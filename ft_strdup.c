c9c1aaca875747e9de3ea78e5dc005903d05174c
LorBN <57272360+LorBN@users.noreply.github.com>
2019-11-01 21:58:49 +0100

    Add files via upload

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduhamel <lduhamel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/13 18:09:22 by lduhamel          #+#    #+#             */
+/*   Updated: 2019/10/21 17:12:57 by lduhamel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*ptr;
+	int		i;
+
+	ptr = NULL;
+	if (!(ptr = (char*)malloc(sizeof(*ptr) * ft_strlen(src) + 1)))
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		ptr[i] = src[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
